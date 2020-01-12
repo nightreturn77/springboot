e11d99463472fb7be1353acfe06b028869c8909d
Victor Dias <victordcporto@gmail.com>
2020-01-12 11:18:01 -0300

    Primeiro spring

---
+++ src/main/java/com/estudando/EstudandoApplication.java
@@ -0,0 +1,13 @@
+package com.estudando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudandoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudandoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estudando/EstudandoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudando;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudandoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
