e23f1a669a7669a81aa075391806f8bb7e0f77b8
Victor Dias <victordcporto@gmail.com>
2020-01-14 10:35:26 -0300

    h2 funcionando

--- src/main/java/com/estudando/entities/User.java
+++ src/main/java/com/estudando/entities/User.java
@@ -1,10 +1,17 @@
 package com.estudando.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User implements java.io.Serializable{
 
 	private static final long serialVersionUID = 1L;
 	
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id; 
 	private String name;
 	private String email;
