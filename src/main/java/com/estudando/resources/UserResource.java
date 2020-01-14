bcfda469d5126eb4bb2aabebf14da791d3ff9ca6
Victor Dias <victordcporto@gmail.com>
2020-01-14 12:43:37 -0300

    json e findbyid

--- src/main/java/com/estudando/resources/UserResource.java
+++ src/main/java/com/estudando/resources/UserResource.java
@@ -1,21 +1,36 @@
 package com.estudando.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.estudando.entities.User;
+import com.estudando.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
-	User u = new User(1L, "Victor", "victor@hotmail.com", "213559697", "1234678");
+	@Autowired
+	private UserService service; 
 	
 	@GetMapping
-	public ResponseEntity<User> findAll(){ 
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll(){ 
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+		
+	}
+	
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 		
 	}
 	
