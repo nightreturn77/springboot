39c8a053079a2654c321ab5a35a5aa421a403991
Victor Dias <victordcporto@gmail.com>
2020-01-14 13:57:20 -0300

    table order e foreign key

--- src/main/java/com/estudando/entities/User.java
+++ src/main/java/com/estudando/entities/User.java
@@ -1,9 +1,13 @@
 package com.estudando.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class User implements java.io.Serializable{
@@ -17,6 +21,9 @@ public class User implements java.io.Serializable{
 	private String email;
 	private String phone;
 	private String password;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>(); 
 	
 	
 	public User() { 
@@ -117,6 +124,11 @@ public class User implements java.io.Serializable{
 	public String toString() {
 		return "User [id=" + id + ", name=" + name + ", email=" + email + ", phone=" + phone + ", password=" + password
 				+ "]";
+	}
+
+
+	public List<Order> getOrders() {
+		return orders;
 	}
 	
 	
--- src/main/java/com/estudando/services/UserService.java
+++ src/main/java/com/estudando/services/UserService.java
@@ -3,6 +3,8 @@ package com.estudando.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.OneToMany;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +16,8 @@ public class UserService {
 	
 	@Autowired
 	private UserRepository repository;
+	
+	
 	
 	public List<User> findAll(){ 
 		return repository.findAll();
